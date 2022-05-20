3d5e7cc7f1df2bbd58c46d71ad698a931cf44a57
Rianfiore <rianfiore@gmail.com>
2022-05-20 04:37:41 -0300

    creating pt-BR support

---
+++ src/translate/languages/ptBR.js
@@ -0,0 +1,106 @@
+import background from "../../img/background-ptbr.jpg";
+
+const messages = {
+  ptBR: {
+    translations: {
+      initialScreen: {
+        header: {
+          entryButton: "Entrar",
+          languages: {
+            ptBR: "Português",
+            enUS: "Inglês",
+          },
+        },
+        main: {
+          background: background,
+          section1: {
+            title: "Filmes, séries e muito mais. Sem limites.",
+            subtitle: "Assista onde quiser. Cancele quando quiser.",
+            registerTitle:
+              "Pronto para assistir? Informe seu email para criar ou reiniciar sua assinatura.",
+            placeholderRegisterButton: "Email",
+            registerButton: "Vamos lá",
+          },
+          section2: {
+            title: "Aproveite na TV.",
+            subtitle:
+              "Assista em Smart TVs, PlayStation, Xbox, Chromecast, Apple TV, aparelhos de Blu-ray e outros dispositivos.",
+          },
+          section3: {
+            title: "Baixe séries para assistir offline.",
+            subtitle:
+              "Salve seus títulos favoritos e sempre tenha algo para assistir.",
+          },
+          section4: {
+            title: "Assista quando quiser.",
+            subtitle:
+              "Assista no celular, tablet, Smart TV ou notebook sem pagar a mais por isso.",
+          },
+          section5: {
+            title: "Crie perfis para crianças.",
+            subtitle:
+              "Deixe as crianças se aventurarem com seus personagens favoritos em um espaço feito só para elas, sem pagar a mais por isso.",
+          },
+          section6: {
+            title: "Perguntas frequentes",
+            menu1: {
+              title: "O que é a Netflix?",
+              content:
+                "A Netflix é um serviço de transmissão online que oferece uma ampla variedade de séries, filmes e documentários premiados em milhares de aparelhos conectados à internet. Você pode assistir a quantos filmes e séries quiser, quando e onde quiser, sem comerciais – tudo por um preço mensal bem acessível. Aqui você sempre encontra novidades. A cada semana, adicionamos novas séries e filmes.",
+            },
+            menu2: {
+              title: "Quanto custa a Netflix?",
+              content:
+                "Assista à Netflix no seu celular, tablet, Smart TV, notebook ou aparelho de streaming por uma taxa mensal única. Os planos variam de R$25,90 a R$55,90 por mês. Sem contrato nem taxas extras.",
+            },
+            menu3: {
+              title: "Onde posso assistir?",
+              content:
+                "Assista onde quiser, quando quiser. Faça login com sua conta Netflix em netflix.com para começar a assistir no computador ou em qualquer aparelho conectado à Internet compatível com o aplicativo Netflix, como Smart TVs, smartphones, tablets, aparelhos de streaming e videogames. Você também pode baixar a sua série favorita com o aplicativo Netflix para iOS, Android ou Windows 10. Use downloads para levar a Netflix para onde quiser sem precisar de conexão com a Internet. Leve a Netflix com você para qualquer lugar.",
+            },
+            menu4: {
+              title: "Como faço para cancelar?",
+              content:
+                "A Netflix é flexível. Não há contratos nem compromissos. Você pode cancelar a sua conta na internet com apenas dois cliques. Não há taxa de cancelamento – você pode começar ou encerrar a sua assinatura a qualquer momento.",
+            },
+            menu5: {
+              title: "O que eu posso assistir na Netflix?",
+              content:
+                "A Netflix tem um grande catálogo de filmes, documentários, séries, originais Netflix premiados e muito mais. Assista o quanto quiser, quando quiser.",
+            },
+            menu6: {
+              title: "A Netflix é adequada para crianças?",
+              content:
+                "A experiência infantil da Netflix faz parte da sua assinatura para que as crianças se divirtam em seu próprio espaço com séries e filmes familiares sob a supervisão dos pais. O recurso de controle dos pais, incluso nos perfis para crianças e protegido por PIN, permite restringir a classificação etária do conteúdo que as crianças podem ver e bloquear títulos específicos que você não quer que elas assistam.",
+            },
+          },
+        },
+        footer: {
+          title: "Dúvidas? Ligue 0800 591 8942",
+          ul: {
+            li1: "Perguntas frequentes",
+            li2: "Relações com investidores",
+            li3: "Formas de assistir",
+            li4: "Informações corporativas",
+            li5: "Só na Netflix",
+            li6: "Centro de ajuda",
+            li7: "Carreiras",
+            li8: "Termos de uso",
+            li9: "Entre em contato",
+            li10: "Conta",
+            li11: "Resgatar cartão pré-pago",
+            li12: "Privacidade",
+            li13: "Teste de velocidade",
+            li14: "Media Center",
+            li15: "Comprar cartão pré-pago",
+            li16: "Preferências de cookies",
+            li17: "Avisos legais",
+          },
+          info: "Netflix Brasil",
+        },
+      },
+    },
+  },
+};
+
+export { messages };
