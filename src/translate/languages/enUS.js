6c9f1c1e102a1adf7d29a30503fb6be84a2171c6
Rianfiore <rianfiore@gmail.com>
2022-05-20 04:37:50 -0300

    creating en-US support

---
+++ src/translate/languages/enUS.js
@@ -0,0 +1,106 @@
+import background from "../../img/background-enus.jpg";
+
+const messages = {
+  enUS: {
+    translations: {
+      initialScreen: {
+        header: {
+          entryButton: "Sign In",
+          languages: {
+            ptBR: "Portuguese",
+            enUS: "English",
+          },
+        },
+        main: {
+          background: background,
+          section1: {
+            title: "Unlimited movies, TV shows, and more.",
+            subtitle: "Watch anywhere. Cancel anytime.",
+            registerTitle:
+              "Ready to watch? Enter your email to create or restart your membership.",
+            placeholderRegisterButton: "Email address",
+            registerButton: "Get Started",
+          },
+          section2: {
+            title: "Enjoy on your TV.",
+            subtitle:
+              "Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more.",
+          },
+          section3: {
+            title: "Download your shows to watch offline.",
+            subtitle:
+              "Save your favorites easily and always have something to watch.",
+          },
+          section4: {
+            title: "Watch everywhere.",
+            subtitle:
+              "Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV without paying more.",
+          },
+          section5: {
+            title: "Create profiles for kids.",
+            subtitle:
+              "Send kids on adventures with their favorite characters in a space made just for them—free with your membership.",
+          },
+          section6: {
+            title: "Frequently Asked Questions",
+            menu1: {
+              title: "What is Netflix?",
+              content:
+                "Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices. You can watch as much as you want, whenever you want without a single commercial – all for one low monthly price. There's always something new to discover and new TV shows and movies are added every week!",
+            },
+            menu2: {
+              title: "How much does Netflix cost?",
+              content:
+                "Watch Netflix on your smartphone, tablet, Smart TV, laptop, or streaming device, all for one fixed monthly fee. Plans range from BRL25.90 to BRL55.90 a month. No extra costs, no contracts.",
+            },
+            menu3: {
+              title: "Where can I watch?",
+              content:
+                "Watch anywhere, anytime. Sign in with your Netflix account to watch instantly on the web at netflix.com from your personal computer or on any internet-connected device that offers the Netflix app, including smart TVs, smartphones, tablets, streaming media players and game consoles. You can also download your favorite shows with the iOS, Android, or Windows 10 app. Use downloads to watch while you're on the go and without an internet connection. Take Netflix with you anywhere.",
+            },
+            menu4: {
+              title: "How do I cancel?",
+              content:
+                "Netflix is flexible. There are no pesky contracts and no commitments. You can easily cancel your account online in two clicks. There are no cancellation fees – start or stop your account anytime.",
+            },
+            menu5: {
+              title: "What can I watch on Netflix?",
+              content:
+                "Netflix has an extensive library of feature films, documentaries, TV shows, anime, award-winning Netflix originals, and more. Watch as much as you want, anytime you want.",
+            },
+            menu6: {
+              title: "Is Netflix good for kids?",
+              content:
+                "The Netflix Kids experience is included in your membership to give parents control while kids enjoy family-friendly TV shows and movies in their own space. Kids profiles come with PIN-protected parental controls that let you restrict the maturity rating of content kids can watch and block specific titles you don’t want kids to see.",
+            },
+          },
+        },
+        footer: {
+          title: "Questions? Call 0800 591 8942",
+          ul: {
+            li1: "FAQ",
+            li2: "Investor Relations",
+            li3: "Ways to Watch",
+            li4: "Corporate Information",
+            li5: "Only on Netflix",
+            li6: "Help Center",
+            li7: "Jobs",
+            li8: "Terms of Use",
+            li9: "Contact Us",
+            li10: "Account",
+            li11: "Redeem Gift Cards",
+            li12: "Privacy",
+            li13: "Speed Test",
+            li14: "Media Center",
+            li15: "Buy Gift Cards",
+            li16: "Cookies Preferences",
+            li17: "Legal Notices",
+          },
+          info: "Netflix Brazil",
+        },
+      },
+    },
+  },
+};
+
+export { messages };
