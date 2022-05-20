ff40f21aedbd94f097cadf91d7663b4448cebea7
Rianfiore <rianfiore@gmail.com>
2022-05-20 04:37:18 -0300

    creating messages for each language

---
+++ src/translate/languages/index.js
@@ -0,0 +1,9 @@
+import { messages as portugueseMessages } from "./ptBR";
+import { messages as englishMessages } from "./enUS";
+
+const messages = {
+  ...portugueseMessages,
+  ...englishMessages,
+};
+
+export { messages };
