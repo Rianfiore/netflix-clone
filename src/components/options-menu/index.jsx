37aba9e37734e118ae53bf02d1f7a459b42afd4c
Rianfiore <rianfiore@gmail.com>
2022-05-20 04:35:23 -0300

    creating options menu

---
+++ src/components/options-menu/index.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { i18n } from "../../translate/i18n";
+import * as S from "./styles";
+import Select from "../select";
+
+const OptionsMenu = ({ type }) => {
+  function translate(phrase) {
+    return i18n.t(`initialScreen.header.${phrase}`);
+  }
+
+  const Primary = () => {
+      return (
+        <S.Primary>
+          <Select type="language" options={["ptBR", "enUS"]}/>
+          <button type="button" class="btn btn-danger shadow-none">
+            {translate("entryButton")}
+          </button>
+        </S.Primary>
+      );
+    },
+    Secondary = () => {
+      return (
+        <S.Secondary>
+          <Select type="language" options={["ptBR", "enUS"]}/>
+        </S.Secondary>
+      );
+    };
+
+  switch (type) {
+    case "primary":
+      return <Primary />;
+    case "secondary":
+      return <Secondary />;
+    default:
+      break;
+  }
+};
+
+export default OptionsMenu;
---
+++ src/components/options-menu/styles.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Primary = styled.div`
+  display: flex;
+  justify-content: space-between;
+  gap: 35px;
+  align-items: center;
+
+  button {
+    padding: 7px 17px;
+    width: 100%;
+  }
+`;
+
+export const Secondary = styled.div`
+  display: flex;
+  justify-content: space-between;
+  gap: 35px;
+  align-items: center;
+
+  button {
+    padding: 7px 17px;
+  }
+`;
