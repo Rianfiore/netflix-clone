7e91c76aeaf1875702822e90dd2d059f9d4602a1
Rianfiore <rianfiore@gmail.com>
2022-06-04 21:14:15 -0300

    using HttpApi from i18n-http-backend

--- src/translate/i18n.js
+++ src/translate/i18n.js
@@ -1,14 +1,17 @@
 import i18n from "i18next";
+import HttpApi from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { messages } from "./languages";
 
+i18n
+  .use(HttpApi)
+  .use(LanguageDetector)
+  .init({
+    debug: false,
+    defaultNS: ["translations"],
 
-i18n.use(LanguageDetector).init({
-  debug: false,
-  defaultNS: ["translations"],
-
-  ns: ["translations"],
-  resources: messages,
-});
+    ns: ["translations"],
+    resources: messages,
+  });
 
 export { i18n };
