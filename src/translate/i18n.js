cb4d2d8b12f221e39e29329b360157f10ca40abf
Rianfiore <rianfiore@gmail.com>
2022-06-04 22:27:06 -0300

    adding automatic language detect from browser

--- src/translate/i18n.js
+++ src/translate/i18n.js
@@ -1,17 +1,14 @@
 import i18n from "i18next";
-import HttpApi from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { messages } from "./languages";
 
-i18n
-  .use(HttpApi)
-  .use(LanguageDetector)
-  .init({
-    debug: false,
-    defaultNS: ["translations"],
+i18n.use(LanguageDetector).init({
+  debug: false,
+  defaultNS: ["translations"],
+  ns: ["translations"],
+  resources: messages,
+});
 
-    ns: ["translations"],
-    resources: messages,
-  });
+i18n.translator.language = localStorage.getItem("i18nextLng").replace("-", "");
 
 export { i18n };
