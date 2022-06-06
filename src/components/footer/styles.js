020b7ede468a2d2078d87a2ab521e25acde47698
Rianfiore <rianfiore@gmail.com>
2022-06-05 23:06:06 -0300

    styling footer

--- src/components/footer/styles.js
+++ src/components/footer/styles.js
@@ -1,5 +1,31 @@
 import styled from "styled-components";
 
-export const Container = styled.footer`
+export const Container = styled.div`
   background-color: black;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+
+  div {
+    width: 50%;
+
+    nav {
+      ul {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr 1fr;
+        padding: 0px;
+
+        div {
+          width: 100%;
+          line-height: 200%;
+
+          li {
+            list-style: none;
+            text-align: left;
+          }
+        }
+      }
+    }
+  }
 `;
